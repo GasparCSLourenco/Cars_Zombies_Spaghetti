5d1467277f8d77aec9e0c29211cbb0494ddb5ff7
GasparCSLourenco <gasplourenco@gmail.com>
2025-07-10 21:52:54 -0400

    Update ProcessingAppliances.cs

--- Assets/Scripts/Objects Scripts/ProcessingAppliances/ProcessingAppliances.cs
+++ Assets/Scripts/Objects Scripts/ProcessingAppliances/ProcessingAppliances.cs
@@ -5,6 +5,17 @@ using UnityEngine.Events;
 
 public class ProcessingAppliances : MonoBehaviour, IInteractable
 {
+	public UnityAction<IInteractable> OnInteractionComplete { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+	public void EndInteraction()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void Interact(Interactor interactor, out bool interactionSuccessful)
+	{
+		throw new System.NotImplementedException();
+	}
 
 
 	// Start is called before the first frame update
